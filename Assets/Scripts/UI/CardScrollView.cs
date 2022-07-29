82d064f95ef0f40583aee8ae6b9ba5efda289960
Lord-Stark <2363173126@qq.com>
2022-07-29 09:52:52 +0800

    modify card scroll view

--- Assets/Scripts/UI/CardScrollView.cs
+++ Assets/Scripts/UI/CardScrollView.cs
@@ -7,32 +7,71 @@ public class CardScrollView : MonoBehaviour
     private Vector3 displayPos;     // The position of the card scroll view when it is displayed
     private Vector3 hiddenPos;     // The position of the card scroll view when it is hidden
 
+    private Vector3 destPos;
+
+    private bool needMoving = false;
+    private float lastMoveTime = 0;
+
+
+    private const float hiddenXDis = 6f;
+
+
+    private const float moveTimeSpan = 0.05f;
+    private const float disDeviation = 1e-6f;
 
     private void Start()
     {
         displayPos = transform.position;
-        hiddenPos = new Vector3(transform.position.x - 10f, transform.position.y, transform.position.z);
+        destPos = displayPos;
+        hiddenPos = new Vector3(transform.position.x - hiddenXDis, transform.position.y, transform.position.z);
+    }
+
+    private void Update()
+    {
+        MoveToDestPos();
     }
 
     public void Display()
     {
-        transform.position = displayPos;
-        // if (transform.position.x < displayPos.x)
-        // {
-        //     transform.position = new Vector3(transform.position.x + 0.1f, transform.position.y, transform.position.z);
-        //     Invoke("Display", 0.1f);
-        // }
-        // else transform.position = displayPos;
+        ChangeDestPos(displayPos);
     }
 
     public void Hide()
     {
-        transform.position = hiddenPos;
-        // if (transform.position.x > hiddenPos.x)
-        // {
-        //     transform.position = new Vector3(transform.position.x - 0.1f, transform.position.y, transform.position.z);
-        //     Invoke("Hide", 0.1f);
-        // }
-        // else transform.position = hiddenPos;
+        ChangeDestPos(hiddenPos);
+    }
+
+    private void ChangeDestPos(Vector3 pos)
+    {
+        needMoving = true;
+        destPos = pos;
+    }
+
+    private void MoveToDestPos()
+    {
+        // if there is no need to move card scroll view, return 
+        if(!needMoving) return;
+
+        // get current time
+        float currTime = TimeManager.instance.GetTimeSecond();
+
+        // if current time is before scheduled time, return 
+        if(currTime - lastMoveTime < moveTimeSpan) return;
+
+        // if card scroll view is close to destination position, change its status
+        if(Mathf.Abs(transform.position.x - destPos.x) <= disDeviation)
+        {
+            transform.position = destPos;
+            needMoving = false;
+            lastMoveTime = 0;
+            return;
+        }
+
+        // else move card scroll view
+        // x2 + (x2 - x1 ) * moveTimeSpan
+        Vector3 newPos = transform.position;
+        newPos.x += (destPos.x - transform.position.x) *moveTimeSpan;
+        transform.position = newPos;
+        lastMoveTime = currTime;
     }
 }
